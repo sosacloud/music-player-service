5b667e19929caf6e06c67020ef9e76cffbd3f5d2
スティーブンさん <underyourcover@icloud.com>
2019-06-21 01:42:11 -0700

    configured init class rendering

---
+++ client/src/components/MusicPlayer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MusicPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Music Player Service</h1>
+      </div>
+    );
+  }
+}
+
+export default MusicPlayer;
