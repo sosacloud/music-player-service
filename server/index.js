027ed0a0f72af4ed05688a79a7b8d983a5cb69bc
スティーブンさん <underyourcover@icloud.com>
2019-06-21 01:51:14 -0700

    removed app.use endpoint argument

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(
   })
 );
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/api/songs', (req, res) => {
   res.sendStatus(200);
