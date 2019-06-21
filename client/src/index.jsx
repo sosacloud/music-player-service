6d032265cbe685d0eb3dc0ebc56b4eb1ffd5af80
スティーブンさん <underyourcover@icloud.com>
2019-06-21 01:40:04 -0700

    init ReactDOM

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MusicPlayer from './components/MusicPlayer.jsx';
+
+ReactDOM.render(<MusicPlayer />, document.getElementById('app'));
