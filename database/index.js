8f4f20c915134ff7cea696b8f8ac2d1e0c79157d
スティーブンさん <underyourcover@icloud.com>
2019-06-21 02:30:16 -0700

    created Songs table

--- database/index.js
+++ database/index.js
@@ -12,3 +12,25 @@ db.authenticate()
   .catch(err => {
     throw err;
   });
+
+const Songs = db.define(
+  'Songs',
+  {
+    song_artist: Sequelize.STRING,
+    song_title: Sequelize.STRING,
+    song_url: Sequelize.STRING, // AWS S3
+    user: Sequelize.STRING, // call to Juan's service
+    user_comment: Sequelize.STRING, // call to Juan's service
+    song_release: Sequelize.DATE,
+    song_tags: Sequelize.STRING,
+    song_length: Sequelize.INTEGER, // make sure to render proper format on client side using styling or VanillaJS
+    song_thumbnail: Sequelize.STRING // AWS S3
+  },
+  {
+    timestamps: false
+  }
+);
+
+db.sync();
+
+module.exports = { Songs };
