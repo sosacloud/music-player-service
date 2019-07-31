edd5f55782b20b8e86b44804121091ed42a7c990
スティーブンさん <underyourcover@icloud.com>
2019-07-30 23:23:08 -0700

    updated number of songs

--- database/seed.js
+++ database/seed.js
@@ -2,16 +2,8 @@ const db = require('./index');
 const faker = require('faker');
 
 const songs = [
-  'https://music-player-service.s3-us-west-1.amazonaws.com/7+rings.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/bloodline.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/cant+believe+the+way+we+flow.mp3',
   'https://music-player-service.s3-us-west-1.amazonaws.com/ill+come+too.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/imagine.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/mile+high.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/nasa.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/needy.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/power+on.mp3',
-  'https://music-player-service.s3-us-west-1.amazonaws.com/tell+them.mp3'
+  'https://music-player-service.s3-us-west-1.amazonaws.com/imagine.mp3'
 ];
 
 const randomNum = (min, max) => {
@@ -26,7 +18,7 @@ for (let i = 0; i <= 99; i++) {
     song_artist: faker.random.word(),
     song_title: `${faker.random.word()} ${faker.random.word()}`,
     song_album: `${faker.random.word()} ${faker.random.word()}`,
-    song_url: songs[randomNum(0, 10)],
+    song_url: songs[randomNum(0, 1)],
     user: faker.internet.userName(),
     user_comment: faker.lorem.sentence(),
     song_release: faker.date.past(),
